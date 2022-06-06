6be851f6da42267c5a445b666f76a5fc036c38c0
eduipdados <contatoswnet@gmail.com>
2022-06-06 17:34:01 -0300

    Estrutura pronta

--- Api.Domain/Interfaces/IRepository.cs
+++ Api.Domain/Interfaces/IRepository.cs
@@ -1,16 +1,19 @@
 using Api.Domain.Entidades;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Api.Domain.Interfaces
 {
-    public interface IRepository<T> where T:BaseEntity
+    public interface IRepository<T> where T : BaseEntity
     {
         Task<T> InsertAsync (T item);
         Task<T> UpdateAsync (T item);
         Task<bool> DeleteAsync (Guid Id);
         Task<T> SelectAsync (Guid Id);
-        Task<IEnumerable> SelectAsync();
+        Task<IEnumerable<T>> SelectAsync();
+        Task<bool> ExistAsync(Guid id);
+
     }
 }
